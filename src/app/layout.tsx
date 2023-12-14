5803d8139672b40a180863784b9d2b3e6271e5be
Jasper Hsiao <orz2931@gmail.com>
2023-12-14 12:45:13 +0800

    delete import SignInProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navigation from "@/components/navigation/navigation";
 
-import { SignInProvider, SignIn } from "@/components/sign-in/signin";
+import { SignIn } from "@/components/sign-in/signin";
 
 const inter = Inter({ subsets: ["latin"] });
 
